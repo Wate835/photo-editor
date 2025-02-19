7a4a92ec7a4a858aac35996352cb62d30d68ce21
Wate835 <santahoe@mail.ru>
2025-02-19 12:10:19 +0300

    Updates

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { createApp } from "vue";
-import VueKonva from "vue-konva";
-import './style.css'
-import App from "./App.vue";
-
-const app = createApp(App);
-app.use(VueKonva);
-
-app.mount("#app");
